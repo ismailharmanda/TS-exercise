2af1107c30232e4a31b6f6237b175e5b18a824e5
ismail harmanda <iharmanda@hotmail.com>
2021-10-27 21:40:34 +0300

    Instance Method Modifiers

--- classes.ts
+++ classes.ts
@@ -1,23 +1,19 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  protected honk(): void {
     console.log("beep");
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+  public startDrivingProcess(): void {
+    this.drive();
+    this.honk;
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
-
 const car = new Car();
 
-car.drive();
-car.honk();
+car.startDrivingProcess();
