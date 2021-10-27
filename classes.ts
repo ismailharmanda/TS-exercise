9798a5f25432fef0f83a24652a58efb03ec99ab1
ismail harmanda <iharmanda@hotmail.com>
2021-10-27 22:10:25 +0300

    Fields with Inheritance

--- classes.ts
+++ classes.ts
@@ -10,15 +10,20 @@ const vehicle = new Vehicle("orange");
 console.log(vehicle.color);
 
 class Car extends Vehicle {
+  constructor(public wheels: number, public color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log("vroom");
   }
   public startDrivingProcess(): void {
     this.drive();
     this.honk();
+    console.log(this.color);
+    console.log(this.wheels);
   }
 }
 
-const car = new Car("orange");
+const car = new Car(4, "pink");
 
 car.startDrivingProcess();
