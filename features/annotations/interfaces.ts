4321f6ba225fed85c18c99aad3c7cb7bc77d0a3b
ismail harmanda <iharmanda@hotmail.com>
2021-10-27 17:39:37 +0300

    functions

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,19 +1,18 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
-const oldCivic: Vehicle = {
+const oldCivic = {
   name: "civic",
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
