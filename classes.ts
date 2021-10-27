74fb3ec088db797adab1775bfaf3d9a5dc633753
ismail harmanda <iharmanda@hotmail.com>
2021-10-27 21:28:46 +0300

    basic inheritence

---
+++ classes.ts
@@ -0,0 +1,23 @@
+class Vehicle {
+  drive(): void {
+    console.log("chugga chugga");
+  }
+  honk(): void {
+    console.log("beep");
+  }
+}
+
+class Car extends Vehicle {
+  drive(): void {
+    console.log("vroom");
+  }
+}
+
+const vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
+
+const car = new Car();
+
+car.drive();
+car.honk();
