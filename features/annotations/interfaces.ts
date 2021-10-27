a4cd9790a5aad7859f6359fcad1a9c22163c5564
ismail harmanda <iharmanda@hotmail.com>
2021-10-27 18:19:11 +0300

    code reuse with interfaces

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -2,6 +2,15 @@ interface Reportable {
   summary(): string;
 }
 
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  },
+};
+
 const oldCivic = {
   name: "civic",
   year: new Date(),
@@ -16,3 +25,4 @@ const printSummary = (item: Reportable): void => {
 };
 
 printSummary(oldCivic);
+printSummary(drink);
